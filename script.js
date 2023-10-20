1b40172fc77b564499aa70f7980a20f93e287c56
castitasa <aibike0806@gmail.com>
2023-10-21 00:22:10 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,47 @@
+const containerEl = document.querySelector(".container");
+const choice = document.querySelector(".choice");
+const block = document.querySelector(".block");
+
+fetch("https://jsonplaceholder.typicode.com/posts")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+
+    data.map((el) => {
+      const wrapper = document.createElement("div");
+      wrapper.classList.add("wrapper");
+      wrapper.innerHTML += `
+        <h1>${el.id}</h1>
+        </br>
+        <p><b>userId</b>: ${el.userId}</p>
+        <p><b>title</b>: ${el.title}</p>
+        <p><b>body</b>: ${el.body}</p>`
+      containerEl.appendChild(wrapper);
+    });
+
+    const btn = document.querySelector('#btn');
+    btn.addEventListener("click", () => {
+      const inputValue = document.querySelector('input').value;
+
+      console.log(data[inputValue]);
+
+        if (data[inputValue] !== undefined) {
+          const el = data[inputValue - 1]
+          const wrapper = document.createElement("div");
+          wrapper.classList.add("wrapper");
+
+          wrapper.innerHTML += `
+        <h1>${el.id}</h1>
+        </br>
+        <p><b>userId</b>: ${el.userId}</p>
+        <p><b>title</b>: ${el.title}</p>
+        <p><b>body</b>: ${el.body}</p>`
+          block.appendChild(wrapper);
+        } else {
+          alert("Такого id нет")
+        }
+      
+    });
+  })
